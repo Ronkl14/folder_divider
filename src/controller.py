fafb8b165f023ff17df388f632a56e7d4742c110
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2024-08-15 14:02:18 +0300

    fix progress bar bug in sequential mode

--- src/controller.py
+++ src/controller.py
@@ -3,12 +3,13 @@ import shutil
 import math
 import threading
 
-def copy_files_to_folder(group, source_folder, target_folder, progress_bar=None):
+def copy_files_to_folder(group, source_folder, target_folder, progress_bar):
     total_files = len(group)
     for idx, file_name in enumerate(group):
         shutil.copy(os.path.join(source_folder, file_name), target_folder)
         if progress_bar:
-            progress_bar.value = (idx + 1) / total_files  
+            progress_bar.value = (idx + 1) * 0.01
+            progress_bar.page.update()
 
 def copy_unknown_file(unknown_file, source_folder, target_folders):
     for target_folder in target_folders:
@@ -33,14 +34,16 @@ def divide_and_copy_files(source_folder: str, target_folders: list, parallel_cop
     else:
         total_files = len(raw_files)
         for i, target_folder in enumerate(target_folders):
+            progress_bars[i].value = 0
+            page.update()  
+
             group = raw_files[i*group_size : (i+1)*group_size]
             copy_files_to_folder(group, source_folder, target_folder, progress_bars[i])
-            # Update progress bar after each folder
-            progress = (i + 1) / num_groups
-            progress_bars[i].value = progress
+
+            progress_bars[i].value = 1.0
             page.update()
 
     unknown_files = [f for f in os.listdir(source_folder) if not f.endswith('.raw')]
     if unknown_files:
-        unknown_file = unknown_files[0]  # Assuming there is only one unknown file
+        unknown_file = unknown_files[0]
         copy_unknown_file(unknown_file, source_folder, target_folders)
